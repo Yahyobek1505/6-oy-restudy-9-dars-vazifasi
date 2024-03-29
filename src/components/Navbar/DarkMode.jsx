4b22ea596fc7e1b272d7f888f4669bc73b18c5be
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-03-29 12:25:23 +0500

    Header Section is done!

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import LightButton from '../../assets/web-site/light-mode-button.png'
 import DarkButton from '../../assets/web-site/dark-mode-button.png'
 function DarkMode() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+  const element = document.documentElement;
+  useEffect(() =>{
+  if (theme == "dark") {
+    element.classList.add("dark");
+    localStorage.setItem("theme", "dark");    
+  }else{
+    element.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+
+  }
+  }, [theme])
   return (
-    <div>
-      <img src={LightButton} alt="" />
+    <div className='relative'>
+      <img src={LightButton}  alt="" 
+      onClick={() => setTheme(theme =="light" ? "dark" : "light")}
+      className= {`w-12 cursor-pointer drop-shadow [1px_1px_1px_rgba(0, 0, 0, 0.1)] transition-all duration-300 absolute right-0 z-10  ${theme == "dark" ? "opacity-0" : "opacity-100"}`}/>
+      <img src={DarkButton} alt="" 
+      onClick={() => setTheme(theme =="light" ? "dark" : "light")}
+      className='w-12 cursor-pointer drop-shadow [1px_1px_1px_rgba(0, 0, 0, 0.1)] transition-all duration-300'/>
     </div>
   )
 }
