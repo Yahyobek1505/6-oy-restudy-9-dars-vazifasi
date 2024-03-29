47d2a5496e332238d84c6e5e9792a5f5e185bfbc
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-03-29 08:16:30 +0500

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar></Navbar>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LightButton from '../../assets/web-site/light-mode-button.png'
+import DarkButton from '../../assets/web-site/dark-mode-button.png'
+function DarkMode() {
+  return (
+    <div>
+      <img src={LightButton} alt="" />
+    </div>
+  )
+}
+
+export default DarkMode
+
+
